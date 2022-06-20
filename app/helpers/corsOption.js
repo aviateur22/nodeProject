62938fb7a21667086d548977c1b709b1ef3d06cb
cyrille cade <42223528+aviateur22@users.noreply.github.com>
2022-06-20 14:00:17 +0200

    Update corsOption.js

--- app/helpers/corsOption.js
+++ app/helpers/corsOption.js
@@ -1,18 +1,7 @@
 /**Configurations des CORS */
-module.exports= ()=>{
-    let corsOption = {        
+let corsOption = {        
         credentials: true,
-        methods: 'GET,PUT,PATCH,POST,DELETE'
-    };
-    /** Mode Développement  */
-    if(process.env.NODE_ENV==='DEV'){
-        console.log('en dev');
-        const CORS_ORIGIN = process.env.CORS_ORIGIN;
-        corsOption.origin= CORS_ORIGIN.split(',');        
-    } else /**mode PRDODUCTION */    
-    {        
-        console.log('en prod');
-        corsOption.origin= process.env.CORS_ORIGIN.split(',');
-    }
-    return corsOption;
+        methods: 'GET,PUT,PATCH,POST,DELETE',
+        origin: process.env.CORS_ORIGIN.split(',')
 };
+return corsOption;
