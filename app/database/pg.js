12f4346defcde75a0c6f4007757951dc9c8483e0
cyrille cade <42223528+aviateur22@users.noreply.github.com>
2022-07-13 17:56:01 +0200

    Update pg.js

--- app/database/pg.js
+++ app/database/pg.js
@@ -1,17 +1,17 @@
- const { Pool } = require('pg');
+const { Pool } = require('pg');
 
-  const config = {};
-  /**la variable NODE_ENV est inclue dans HEROKU */
-  if(process.env.NODE_ENV === 'production') {
-      console.log( `database.js - ENV: production - DATABASE: ${process.env.DATABASE_URL}`)
-      config.connectionString = process.env.DATABASE_URL;
-      config.ssl = {
-          rejectUnauthorized: false,
-       }
-   } else {
-       console.log( `database.js - ENV: DEV - DATABASE: ${process.env.DATABASE_URL}`)
-       config.connectionString = process.env.DATABASE_URL;       
-   };
+const config = {};
+/**la variable NODE_ENV est inclue dans HEROKU */
+if(process.env.NODE_ENV === 'production') {
+    console.log( `database.js - ENV: production - DATABASE: ${process.env.DATABASE_URL}`);
+    config.connectionString = process.env.DATABASE_URL;
+    config.ssl = {
+        rejectUnauthorized: false,
+    };
+} else {
+    console.log( `database.js - ENV: DEV - DATABASE: ${process.env.DATABASE_URL}`);
+    config.connectionString = process.env.DATABASE_URL;       
+}
 
 const pool = new Pool(config);
 module.exports = pool;
